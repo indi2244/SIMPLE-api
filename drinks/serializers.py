dd6112fff9670277ac26352cee930f68dc4e010c
indira <kasichhwaindira@gmail.com>
2024-08-16 18:33:33 +0545

    first commit

---
+++ drinks/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import drink
+class drinkserializer(serializers.ModelSerializer):
+    class Meta:
+        model  = drink
+        fields = ['id' , 'name' , 'description']
